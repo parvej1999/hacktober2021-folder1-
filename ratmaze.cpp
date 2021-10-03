78a26a74d139db0e928daa8fabc7e5a6dcb569a2
djangomonu <73739122+djangomonu@users.noreply.github.com>
2021-10-03 13:45:27 +0530

    completed successfully

--- ratmaze.cpp
+++ ratmaze.cpp
@@ -34,3 +34,36 @@ bool solvableMaze(int maze[][len], int sx, int sy, int dx, int dy){
 
     return false;
 }
+
+
+void countAllPaths(int maze[][len], int sx, int sy, int dx, int dy, int &count){
+    if(sx<0 or sy<0 or dx>3 or dy>3 or maze[sx][sy] == 0 or vis[sx][sy] == 1)return ;
+
+    if(sx == dx and sy == dy){
+        count += 1;
+        
+    }
+    vis[sx][sy] = 1;
+    countAllPaths(maze, sx, sy-1, dx, dy, count);
+
+    countAllPaths(maze, sx, sy+1, dx, dy, count);
+
+    countAllPaths(maze, sx+1, sy, dx, dy, count);
+
+    countAllPaths(maze, sx-1, sy, dx, dy, count);
+
+    vis[sx][sy] = 0;
+}
+
+int main(){
+    int maze[4][4] = {  {1, 0, 0 ,0}, 
+                        {1, 1, 0, 0}, 
+                        {0, 1, 1, 0}, 
+                        {0, 0, 1, 1}};
+    int count = 0;
+    cout<<solvableMaze(maze, 0, 0, 3, 3)<<endl;
+    // countAllPaths(maze, 0, 0, 3, 3, count);
+    // cout<<count<<endl;
+
+    return 0;
+}
