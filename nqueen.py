b53fb87cd9952405219bf730fb9f83a21d507198
HackIfying <92533417+HackIfying@users.noreply.github.com>
2021-10-14 20:23:51 +0530

    Update nqueen.py

--- nqueen.py
+++ nqueen.py
@@ -1,1 +1,16 @@
 #help in creating nqueen in python 
+import os
+import time
+
+def printBoard(board, n):
+    string = "-----"*n
+    for i in range(0, n):
+        if(i==0):
+            print(string)
+        for j in range(0, n):
+            if(j==0):
+                print("|", end=" ")
+            print(board[i][j], end=" | ")
+        print()
+        # print("---------------------------------")
+        print(string)
